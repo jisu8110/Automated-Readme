9c044c22c597ab0c3a08c9795783f12ba2754276
unknown <jisu811023@gmail.com>
2024-04-17 16:38:40 +0900

    [try] read first line

--- docs/py_example4-2.py
+++ docs/py_example4-2.py
@@ -1,3 +1,3 @@
 # this file is py_example 4-2 first line 
 # py_example 4-2 second line
-# py_example 4-2 third line
+# py_example 4-2 third line sdf
---
+++ docs/py_example4-3.py
@@ -0,0 +1,3 @@
+# this file is py_example 4-3 first line 
+# py_example 4-3 second line
+# py_example 4-3 third line
