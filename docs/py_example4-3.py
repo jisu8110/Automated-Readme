58eee6c9d6645a12cdf385c291ae04a616febd3e
unknown <jisu811023@gmail.com>
2024-04-17 16:52:53 +0900

    [try] read first line

--- docs/py_example4-3.py
+++ docs/py_example4-3.py
@@ -1,3 +1,3 @@
-# this file is py_example 4-3 first line 
+# this file is py_example 4-3 first line  MODI
 # py_example 4-3 second line
 # py_example 4-3 third line
---
+++ docs/py_example4-4.py
@@ -0,0 +1,3 @@
+# this file is py_example 4-4 first line 
+# py_example 4-4 second line
+# py_example 4-4 third line
