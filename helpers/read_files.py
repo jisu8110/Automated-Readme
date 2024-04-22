22a58d81356ece7ef91d007a3268249df3d183f4
unknown <jisu811023@gmail.com>
2024-04-22 17:19:04 +0900

    delete files

--- helpers/read_files.py
+++ helpers/read_files.py
@@ -18,24 +18,15 @@ def extract_table_data(markdown_text):
     table_data = [(name.strip(), description.strip()) for name, description, _ in matches]
     return table_data
 
-def add_line(column_names, file_paths):
-    print("[[[ fisrt file_paths ]]]")
-    print(type(file_paths))
-    print(len(file_paths))
-    print(file_paths)
-    file_paths_list = file_paths.split()
-
+def group_files_by_directory(file_paths_list):
     dir_groups = {}
-    
-    print("[[[ for file_paths ]]]")
     for file_path in file_paths_list:
-        print(file_path)
         dir_name = os.path.dirname(file_path)
         dir_groups.setdefault(dir_name, []).append(file_path)
-    print("[[[ dir_groups ]]]")
-    print(dir_groups)
+    return dir_groups
 
-    for dir_name, file_paths_list in dir_groups.items():
+def add_line(column_names, added_dir_groups):
+    for dir_name, file_paths_list in added_dir_groups.items():
         file_names = []
         first_lines = []
 
@@ -59,7 +50,8 @@ def add_line(column_names, file_paths):
                 table_data = extract_table_data(markdown_text)
                 df_original = pd.DataFrame(table_data, columns=column_names)
 
-                file_info = ["[{}]({})".format(file_name, file_path) for file_name, file_path in zip(file_names, file_paths_list)]
+                ###############################################
+                file_info = ["[{}](./{})".format(file_name, file_name) for file_name in zip(file_names, file_names)]
                 print("[[[ file_info ]]]")
                 print(file_info)
                 print("[[[ first_lines ]]]")
@@ -69,6 +61,8 @@ def add_line(column_names, file_paths):
                     column_names[1]: first_lines 
                 })
                 df_original = pd.concat([df_original, df_new]).sort_values(by=column_names[0]).reset_index(drop=True)
+                ###############################################
+            
                 df_markdown = df_original.to_markdown(index=False)
 
             with open(readme_path, 'w+') as file:
@@ -76,28 +70,111 @@ def add_line(column_names, file_paths):
         else:
             print(f"There's no file in {file_path}")
 
+def delete_line(column_names, deleted_dir_groups):
+    for dir_name, file_paths_list in deleted_dir_groups.items():
+        file_names = []
 
+        for file_path in file_paths_list:
+            file_name = os.path.basename(file_path)
+            file_names.append(file_name)
 
-# def delete_line(column_names, file_paths):
+        readme_path = os.path.join(dir_name, "README.md")
+        if os.path.exists(readme_path):
+            print(f"=== open readme: {file_path}")
+            with open(readme_path, 'r') as file:
+                markdown_text = file.read()
+                table_data = extract_table_data(markdown_text)
+                df_original = pd.DataFrame(table_data, columns=column_names)
 
-#     for file_path in file_paths:
+                ###############################################
+                for file_name in file_names:
+                    df_original = df_original[~df_original[column_names[0]].str.contains(f"[{file_name}]")]
+                ###############################################
+            
+                df_markdown = df_original.to_markdown(index=False)
 
-#         ###### 리드미에서 삭제
-#         ###### ==============================
+            with open(readme_path, 'w+') as file:
+                file.write(df_markdown)
+        else:
+            print(f"There's no file in {file_path}")
 
-# def modify_line(column_names, file_paths):
+def modify_line(column_names, modified_dir_groups):
+    for dir_name, file_paths_list in modified_dir_groups.items():
+        file_names = []
+        first_lines = []
 
-#     for file_path in file_paths:
+        for file_path in file_paths_list:
+            file_name = os.path.basename(file_path)
+            file_names.append(file_name)
 
-#         ###### 리드미에서 수정
-#         ###### ==============================
+            if os.path.exists(file_path):
+                print(f"=== open file: {file_path}")
+                with open(file_path, 'r') as file:
+                    first_line = file.readline().strip()
+                    first_lines.append(preprocessing_des(first_line))
+            else:
+                print(f"There's no file in {file_path}")
 
-# def rename_line(column_names, file_paths):
+        readme_path = os.path.join(dir_name, "README.md")
+        if os.path.exists(readme_path):
+            print(f"=== open readme: {file_path}")
+            with open(readme_path, 'r') as file:
+                markdown_text = file.read()
+                table_data = extract_table_data(markdown_text)
+                df_original = pd.DataFrame(table_data, columns=column_names)
 
-#     for file_path in file_paths:
+                ###############################################
+                for i in range(len(file_names)):
+                    target_row = df_original[column_names[0]].str.contains(f"[{file_names[i]}]")
+                    df_original.loc[target_row, column_names[1]] = first_lines[i]
+                ###############################################
+            
+                df_markdown = df_original.to_markdown(index=False)
 
-#         ###### 리드미에서 수정
-#         ###### ==============================
+            with open(readme_path, 'w+') as file:
+                file.write(df_markdown)
+        else:
+            print(f"There's no file in {file_path}")
+
+def rename_line(column_names, renamed_dir_groups):
+    for dir_name, file_paths_list in renamed_dir_groups.items():
+        file_names = []
+        first_lines = []
+
+        for file_path in file_paths_list:
+            file_name = os.path.basename(file_path)
+            file_names.append(file_name)
+
+            if os.path.exists(file_path):
+                print(f"=== open file: {file_path}")
+                with open(file_path, 'r') as file:
+                    first_line = file.readline().strip()
+                    first_lines.append(preprocessing_des(first_line))
+            else:
+                print(f"There's no file in {file_path}")
+
+        readme_path = os.path.join(dir_name, "README.md")
+        if os.path.exists(readme_path):
+            print(f"=== open readme: {file_path}")
+            with open(readme_path, 'r') as file:
+                markdown_text = file.read()
+                table_data = extract_table_data(markdown_text)
+                df_original = pd.DataFrame(table_data, columns=column_names)
+
+                ###############################################
+                file_info = ["[{}](./{})".format(file_name, file_name) for file_name in zip(file_names, file_names)]
+                for i in range(len(file_names)):
+                    target_row = df_original[column_names[1]].str.contains(f"{first_lines[i]}")
+                    df_original.loc[target_row, column_names[0]] = file_info[i]
+                ###############################################
+            
+                df_markdown = df_original.to_markdown(index=False)
+
+            with open(readme_path, 'w+') as file:
+                file.write(df_markdown)
+        else:
+            print(f"There's no file in {file_path}")
+
 
 def main():
     parser = argparse.ArgumentParser(description="Read a first line of the file.")
@@ -107,17 +184,17 @@ def main():
     parser.add_argument("renamed_files", type=str, help="Path to the renamed files")
     args = parser.parse_args()
 
-    # read_line(added_files = args.added_files, 
-    #           deleted_files = args.deleted_files, 
-    #           modified_files = args.modified_files, 
-    #           renamed_files = args.renamed_files,)
-
     column_names = ["File Name", "Description"]
-
-    add_line(column_names, args.added_files)
-    # delete_line(column_names, args.deleted_files)
-    # modify_line(column_names, args.modified_files)
-    # rename_line(column_names, args.renamed_files)
+    
+    added_dir_groups = group_files_by_directory(args.added_files.split())
+    deleted_dir_groups = group_files_by_directory(args.deleted_files.split())
+    modified_dir_groups = group_files_by_directory(args.modified_files.split())
+    renamed_dir_groups = group_files_by_directory(args.renamed_files.split())
+    
+    add_line(column_names, added_dir_groups)
+    delete_line(column_names, deleted_dir_groups)
+    modify_line(column_names, modified_dir_groups)
+    rename_line(column_names, renamed_dir_groups)
     
 
 if __name__ == "__main__":
